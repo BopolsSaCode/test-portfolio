0f42568f37ee43ea55c6f3f4b9aabbe29b7d4ec5
BopolsSaCode <christian_jason_beloso@dlsl.edu.ph>
2023-08-10 21:24:36 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors:{
+      'purple3':'#896bff ',
+      'purple2':'#8a3eff ',
+      'purple': '#9638e5',
+      'dark':'#000000',
+      'light':'#ffffff',
+    },
+  },
+  plugins: [],
+}
+
